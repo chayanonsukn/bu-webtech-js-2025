4d671a8cb7cbd45f58293947a3fa3b7a24639d8f
chayanonsukn <chayanon.sukn@bumail.net>
2025-10-03 11:31:34 +0700

    testjs.js

---
+++ javascirpts/testjs.js
@@ -0,0 +1,11 @@
+let num = prompt('Enter a number')
+
+if (num == 0) {
+    alert("Zero integer number");
+} else if (num % 2 == 0) {
+    alert("Even integer number");
+} else if (num % 2 == 1) {
+    alert("Odd integer number");
+} else {
+    alert("Please Input Integer Number")
+}
